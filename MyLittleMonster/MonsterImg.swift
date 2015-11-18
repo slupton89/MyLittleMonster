ea8f70a487dd06fb5ef35d13092693a701e4e1c6
Shane Lupton <shanelupton@shanes-macbook-pro.local>
2015-11-18 00:30:07 -0700

    Finished Lecture 52. App Complete.

--- MyLittleMonster/MonsterImg.swift
+++ MyLittleMonster/MonsterImg.swift
@@ -7,3 +7,59 @@
 //
 
 import Foundation
+import UIKit
+
+class MonsterImg: UIImageView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        playIdleAnimation()
+    }
+//Animation
+    func playIdleAnimation() {
+        
+        self.image = UIImage(named: "idle1.png")
+        
+        self.animationImages = nil
+        
+        var imgArray = [UIImage]()
+        for var x = 1; x <= 4; x++ {
+            let img = UIImage(named: "idle\(x).png")
+            imgArray.append(img!)
+    }
+    
+        self.animationImages = imgArray
+        self.animationDuration = 0.8
+        self.animationRepeatCount = 0
+        self.startAnimating()
+    
+    }
+
+    func playDeathAnimation() {
+        
+        self.image = UIImage(named: "dead5.png")
+        
+        self.animationImages = nil
+        
+        var imgArrayDead = [UIImage]()
+        for var y = 1; y <= 5; y++ {
+            let img = UIImage(named: "dead\(y).png")
+            imgArrayDead.append(img!)
+        }
+        
+        self.animationImages = imgArrayDead
+        self.animationDuration = 0.8
+        self.animationRepeatCount = 1
+        self.startAnimating()
+        
+    }
+//animation
+
+}
+
+
+
+
